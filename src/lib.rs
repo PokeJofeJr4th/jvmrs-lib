7e1567dc0124cd29788edb941800c519fd1fc3d4
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2024-05-06 10:58:11 -0400

    method handle, constant

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod access;
+mod class;
 mod field;
 mod method;
 
 pub use access::AccessFlags;
+pub use class::{Constant, MethodHandle};
 pub use field::FieldType;
 pub use method::MethodDescriptor;
