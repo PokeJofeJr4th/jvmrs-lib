da0c32ef5ba6470899f8ce9a06850435f7aa0fdd
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2024-05-06 10:59:53 -0400

    expose ClassVersion

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,6 @@ mod field;
 mod method;
 
 pub use access::AccessFlags;
-pub use class::{Constant, MethodHandle};
+pub use class::{Constant, MethodHandle, ClassVersion};
 pub use field::FieldType;
 pub use method::MethodDescriptor;
